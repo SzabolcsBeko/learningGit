fb189a82cbed6f77043fe43cbccddd480bddda05
Szabolcs <szabolcs.beko@gmail.com>
2023-05-30 21:57:38 +0200

    another helper method

--- src/main/java/hu/home/DevClass.java
+++ src/main/java/hu/home/DevClass.java
@@ -5,6 +5,13 @@ public class DevClass {
         DevClass.printMessage("Hello Dev Class!");
     }
     public static void printMessage(String message){
+        StringBuilder sb = new StringBuilder("Szabolcs,");
+        sb.append(message);
+        decoratorPrinter(sb.toString());
+        sb = null;
+    }
+
+    public static void decoratorPrinter(String message){
         System.out.println(message);
     }
 
