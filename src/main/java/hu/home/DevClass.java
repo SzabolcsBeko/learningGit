e732cbb6957c86d0f728e219c6b0e12078ced853
Szabolcs <szabolcs.beko@gmail.com>
2023-05-30 18:50:57 +0200

    first commit on learningGit project

---
+++ src/main/java/hu/home/DevClass.java
@@ -0,0 +1,8 @@
+package hu.home;
+
+public class DevClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Dev Class!");
+    }
+
+}
---
+++ src/test/java/hu/home/DevClassTest.java
@@ -0,0 +1,7 @@
+package hu.home;
+
+public class DevClassTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Dev Test Class!");
+    }
+}
